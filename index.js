6ecb5d66d8446011bebc03794eb6912335d1d098
Shathil015 <shathil1033@gmail.com>
2025-11-10 18:36:56 +0600

    added git gitignore components

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new Object(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
       const result = await productsCollection.insertOne(newProduct);
