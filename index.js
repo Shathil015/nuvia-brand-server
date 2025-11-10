6264449533b2023ba6333976357aeb5b85370c30
Shathil015 <shathil1033@gmail.com>
2025-11-10 18:28:54 +0600

    Added get find post delete operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,10 +8,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-//nuviaDBUsers
-//u1fERxFEUuyjiX1n
 const uri =
-  "mongodb+srv://nuviaDBUsers:u1fERxFEUuyjiX1n@newproject10.iqzban8.mongodb.net/?appName=Newproject10";
+  "mongodb+srv://nuviaDBUsers:xHIQFhPU2jzJAjfv@newproject10.iqzban8.mongodb.net/?appName=Newproject10";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,6 +22,56 @@ const client = new MongoClient(uri, {
 app.get("/", (req, res) => {
   res.send("Simple server is running");
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("nuvia_db");
+    const productsCollection = db.collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+        },
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    //     await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Simple app listening on port ${port}`);
